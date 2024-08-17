fed114d5bfa0a99a00b03869a9adf89483ed41ef
androkingdom <androkingdom1@gmail.com>
2024-08-17 10:44:18 +0530

    Initial commit

--- Practice Set/Chapter5/q_05.c
+++ Practice Set/Chapter5/q_05.c
@@ -4,7 +4,9 @@ int main(){
     // What will the following line produce in a C program:
     int a = 4;
     printf("%d %d %d \n", a, ++a, a++);
-    // Answer - 4, 5, 5 Wrong!
+    // Answer - 4 5 5 for compiler evaluate left to right!!
+
+    // Answer - 6 6 4 for compiler evaluate right to left !! (in general)
 
     return 0;
 }
