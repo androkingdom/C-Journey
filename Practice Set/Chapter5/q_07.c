00cb511829f6392fb9a7616011afeb74cc73e6a3
androkingdom <androkingdom1@gmail.com>
2024-08-17 10:55:58 +0530

    Initial commit

--- Practice Set/Chapter5/q_07.c
+++ Practice Set/Chapter5/q_07.c
@@ -1,15 +1,20 @@
-#include <stdio.h> 
+#include <stdio.h>
 // Write a program using function to print the following pattern (first n lines)
 // *
-// **
 // ***
-int main(){
-    int n = 3;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
+// *****
+int star_pattern(int);
+int star_pattern(int n){
+    int star = 1;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= star; j++){
             printf("*");
         }
         printf("\n");
+        star += 2;
     }
+}
+int main(){
+    star_pattern(3);
     return 0;
 }
